857f6c051940af6d91c46f394d0f34f0aedf278c
Liry24 <47878070+Liry24@users.noreply.github.com>
2024-09-10 10:03:48 +0900

    workflow

--- docs/astro.config.mjs
+++ docs/astro.config.mjs
@@ -6,6 +6,7 @@ import tailwind from '@astrojs/tailwind';
 // https://astro.build/config
 export default defineConfig({
     site: 'http://liria.work',
+    outDir: "./docs",
 
     integrations: [tailwind()]
 });
