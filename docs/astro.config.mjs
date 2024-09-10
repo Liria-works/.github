9bf6b9d6d1b4a71603ba1bbc647668ab7f22ea93
Liry24 <47878070+Liry24@users.noreply.github.com>
2024-09-10 16:14:38 +0900

    dev

--- docs/astro.config.mjs
+++ docs/astro.config.mjs
@@ -1,13 +1,15 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
-import tailwind from '@astrojs/tailwind';
+import tailwind from "@astrojs/tailwind";
+
+import icon from "astro-icon";
 
 // https://astro.build/config
 export default defineConfig({
-    output: 'static',
+    output: "static",
 
-    site: 'http://liria.work',
+    site: "http://liria.work",
 
-    integrations: [tailwind()]
+    integrations: [tailwind({ applyBaseStyles: false }), icon()],
 });
